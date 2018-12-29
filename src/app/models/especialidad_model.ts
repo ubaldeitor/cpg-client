2f5945606744fb4adae6bedd4a6881ed693e260b
Luis Ubaldo Godínez Flores <ubgodinez@gmail.com>
2018-12-29 16:29:25 -0600

    Horario change

--- src/app/models/especialidad_model.ts
+++ src/app/models/especialidad_model.ts
@@ -1,16 +1,9 @@
-import {ContactoModel} from './contacto_model';
-
 export class EspecialidadModel {
 
     public id: number;
     public descripcion: string;
-    public calle_numero: string;
-    public colonia: string;
-    public codigo_postal: string;
-    public alcaldia: string;
-    public ciudad: string;
-    public estado: string;
-    public contactos: Array<ContactoModel>;
-    
+    public created_at: string;
+    public updated_at: string;
+
     public constructor() {}
 }
